dca30557a425c25fcc1f62376779cae1ac4dc0c1
seoulboy <imjang@ucsd.edu>
2024-10-21 20:10:20 +0900

    day 17 - project 1, part two, challenge 2 completed

--- days/16/WeSplit/WeSplit/ContentView.swift
+++ days/16/WeSplit/WeSplit/ContentView.swift
@@ -15,12 +15,15 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeopleIndex + 2)
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentage)
-        
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeopleIndex + 2)
         let amountPerPerson = grandTotal / peopleCount
         return amountPerPerson
     }
@@ -50,6 +53,10 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                }
+                
+                Section("Grand total:") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
                 
                 Section("Amount per person:") {
